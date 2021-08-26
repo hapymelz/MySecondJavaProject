bddbbe24118a8bae9da2f6b33d07b630e79dd756
hapymelz <89086928+hapymelz@users.noreply.github.com>
2021-08-26 09:55:34 -0600

    added onto game

--- src/demo/controller/Runner.java
+++ src/demo/controller/Runner.java
@@ -4,6 +4,7 @@ public class Runner
 {
 	public static void main (String [] args) 
 	{
-		System.out.println("Wow, such amaze much cool");
+		Controller app = new Controller();
+		app.start();
 	}
 }
